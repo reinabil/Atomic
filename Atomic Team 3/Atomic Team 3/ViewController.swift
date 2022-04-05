755fa8748c0692f010c76fe66c62570087bd8c16
Nabil <nabilrei1@gmail.com>
2022-04-05 10:33:27 +0700

    initial project

---
+++ Atomic Team 3/Atomic Team 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Atomic Team 3
+//
+//  Created by Nabil Rei on 05/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
