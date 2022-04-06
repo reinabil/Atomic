f1404cecf448bd7abcd78d5ebe734786e40e6834
hafizhMo <hafizh.hizb@gmail.com>
2022-04-06 22:15:35 +0700

    unsetted home (UI)

---
+++ Atomic Team 3/Atomic Team 3/Controller/HomeUnsettedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeUnsettedViewController.swift
+//  Atomic Team 3
+//
+//  Created by Hafizh Mo on 06/04/22.
+//
+
+import Foundation
+import UIKit
+
+class HomeUnsettedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
