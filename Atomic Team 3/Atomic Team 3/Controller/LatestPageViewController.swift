d4745fa35a5daeaa4232f7c59d4ecf0f3a473b9a
hafizhMo <hafizh.hizb@gmail.com>
2022-04-13 08:30:39 +0700

    fix bug: keyboard overlap

--- Atomic Team 3/Atomic Team 3/Controller/LatestPageViewController.swift
+++ Atomic Team 3/Atomic Team 3/Controller/LatestPageViewController.swift
@@ -49,6 +49,23 @@ class LatestPageViewController: UIViewController {
         
         count = UserDefaults.standard.userStreak?.count ?? 1
         lastDate = UserDefaults.standard.userStreak?.lastDate ?? today
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
     
     @IBAction func updateMyProgressPressed(_ sender: UIButton) {
